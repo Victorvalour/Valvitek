f1043ca8096f3d1dfe3534d754c63aa8455cd675
Eweh Victor <ewehvictor7@gmail.com>
2023-11-22 17:43:04 +0100

    Added: Services

--- src/components/NavModal.jsx
+++ src/components/NavModal.jsx
@@ -15,20 +15,43 @@ const NavModal = ({isVisible, onClose}) => {
         }
     
   return (
-    <div className='fixed top-20 inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center z-10' id="wrapper" onClick={handleClose}
+    <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center z-10' id="wrapper" onClick={handleClose}
    >
 
-        <motion.div className='fixed bg-white w-[75%] h-full pt-10 pl-4'
+        <motion.div className='fixed flex flex-col bg-white w-[75%] gap-10 h-full pt-10 pl-4'
          initial={{ x : -180 }}
          animate={{ x : 0 }}
          transition={{ type: "spring" }}
          exit={{ x : -180 }}
         >
+
+<motion.button 
+        animate={"open"}
+        onClick= {() => onClose()}
+        className="flex flex-col self-end space-y-1.5 mr-3 bg-blue-900 w-fit h-fit py-2 rounded-lg px-2 md:hidden"
+        >
+            <motion.span variants={{
+                closed: { rotate:0, y: 0},
+                open: { rotate:45, y:8},
+            }} 
+            className="w-8 h-[2px] bg-white  block rounded-sm"></motion.span>
+            <motion.span variants={{
+                closed: { opacity: 1},
+                open: { opacity: 0},
+            }} 
+            className="w-8 h-[2px] bg-white  block rounded-sm"></motion.span>
+            <motion.span  variants={{
+                closed: { rotate: 0, y:0},
+                open: { rotate: -45, y:-8},
+            }} 
+             className="w-8 h-[2px] bg-white  block rounded-sm"></motion.span>
+        </motion.button>
+
         <div className='w-[80%] flex flex-col'>
 
-            
-         {  /* <button onClick={() => onClose()} className='text-red-500 bg-white text-2xl place-self-end w-8 rounded-lg font-semibold'>X</button> */}
-           
+
+          
+          
              
 
                 <div className='flex items-center h-10 mb-5 border-b-[1px] border-cyan-600'>
--- src/components/Sevices.jsx
+++ src/components/Sevices.jsx
@@ -1,37 +1,67 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import WebDevImg from '../images/web dev img.jpg'
 
 const Sevices = () => {
   return (
     <div className='mt-8'>
-        <div className='relative bg-black w-fit h-fit py-3 px-6 pr-14 text-white text-xl'>
+        <div className='relative mb-8 bg-black w-fit h-fit py-3 px-6 pr-14 text-white text-xl'>
          <p>Our Services</p>
          <motion.div className='absolute w-10 h-10 bg-white -right-5 top-[6px]'
          animate={{ rotate: 45 }}
          ></motion.div>
          </div>
 
-         <div className='flex flex-col items-center px-8'>
-        <div className='flex flex-col items-center'>
-            <p className='font-gothic'>
-                Web Development
+         <div className='flex flex-col items-center px-6 gap-10'>
+        <div className='flex flex-col items-center bg-teal-200 rounded-xl overflow-hidden border-[1px] border-black shadow-[0px_0px_10px_1px_rgba(0,0,0,0.79)]'>
+        
+      
+            <img src={WebDevImg} alt="" />
+            <div className='flex flex-col px-2 pb-6 pt-6'>
+            <p className='font-ibmPlexMono text-3xl font-semibold mb-5'>
+             WEB DEVELOPMENT
             </p>
-            <img src="" alt="" />
-            <p>We build professional and modern websites for clients and businesses all over the world.</p>
+            <p className='text-lg mb-4'>We build professional and modern websites for clients and businesses all over the world.</p>
+            <button href="" className='self-center bg-blue-800 text-white text-xl w-fit h-fit py-2 px-3 rounded-lg'>
+                Get a website
+                </button>
+            </div>
+          
         </div>
 
-        <div className=''>
-            <p className='font-gothic'>
-                Web Development
+        <div className='flex flex-col items-center bg-teal-200 rounded-xl overflow-hidden border-[1px] border-black shadow-[0px_0px_10px_1px_rgba(0,0,0,0.79)]'>
+        
+      
+            <img src={WebDevImg} alt="" />
+            <div className='flex flex-col px-2 pb-6 pt-6'>
+            <p className='font-ibmPlexMono text-3xl font-semibold mb-5'>
+             MOBILE APP DEVELOPMENT
             </p>
+            <p className='text-lg mb-4'>Our developers are on standby, and ready to deliver to you premiem mobile application to your taste and satisfaction.</p>
+
+            <button href="" className='self-center bg-blue-800 text-white text-xl w-fit h-fit py-2 px-3 rounded-lg'>
+                Build an App
+                </button>
+            </div>
+          
         </div>
 
-        <div className=''>
-            <p className='font-gothic'>
-                Web Development
-            </p>
+        <div className='flex flex-col items-center bg-teal-200 rounded-xl overflow-hidden border-[1px] border-black mb-10 shadow-[0px_0px_10px_1px_rgba(0,0,0,0.79)]'>
+        
+      
+        <img src={WebDevImg} alt="" />
+        <div className='flex flex-col px-2 pb-6 pt-6'>
+        <p className='font-ibmPlexMono text-3xl font-semibold mb-5'>
+         DIGITAL COURSES
+        </p>
+        <p className='text-lg mb-4'>If you're looking to start a career in tech, Valvitek has got you covered. We offer high-end digital courses and one-on-one mentorship in various niche. <br />Including: <br />
+        FullStack Web development, Mobile app development, Data analysis, Cyber security, Digital marketing etc.</p>
+        <button href="" className='self-center bg-blue-800 text-white text-xl w-fit h-fit py-2 px-3 rounded-lg'>
+            Get a website
+            </button>
         </div>
-
+      
+    </div>
         </div>
     </div>
   )
